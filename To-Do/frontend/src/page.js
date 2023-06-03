6409d021b62c0dae6fb6c37c4dd4e4807bbee6a1
Awais8459 <67854951+Awais8459@users.noreply.github.com>
2023-06-04 02:06:39 +0500

    CRUD App using MongoDB

--- To-Do/frontend/src/page.js
+++ To-Do/frontend/src/page.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
-import './Page.css'; // Import a CSS file for styling
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Page.css'
 
-export default function Page() {
+const Page = () => {
   const [todos, setTodos] = useState([]);
-  const [selectedTodo, setSelectedTodo] = useState(null);
-  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
-  const [updateTitle, setUpdateTitle] = useState('');
-  const [updateDescription, setUpdateDescription] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   useEffect(() => {
     fetchTodos();
@@ -15,138 +15,130 @@ export default function Page() {
 
   const fetchTodos = async () => {
     try {
-      const response = await Axios.get('http://localhost:4000/todos');
+      const response = await axios.get('http://localhost:4000/todos');
       setTodos(response.data);
     } catch (error) {
       console.log('Failed to fetch todos:', error);
     }
   };
 
-  const addTodo = async () => {
+  const handleCreateTodo = async () => {
+    if (!title || !description) {
+      alert('Please fill in all fields');
+      return;
+    }
+
     try {
-      const response = await Axios.post('http://localhost:4000/todos', {
-        title: updateTitle,
-        description: updateDescription,
-      });
+      const response = await axios.post('http://localhost:4000/todos', { title, description });
       setTodos([...todos, response.data]);
-      setUpdateTitle('');
-      setUpdateDescription('');
+      setTitle('');
+      setDescription('');
     } catch (error) {
-      console.log('Failed to add todo:', error);
+      console.log('Failed to create todo:', error);
     }
   };
 
-  const updateTodo = async (id) => {
+  const handleUpdateTodo = async () => {
+    if (!title || !description) {
+      alert('Please fill in all fields');
+      return;
+    }
+
     try {
-      const response = await Axios.put(`http://localhost:4000/todos/${id}`, {
-        title: updateTitle,
-        description: updateDescription,
+      const response = await axios.put(`http://localhost:4000/todos/${editingTodo._id}`, {
+        title,
+        description,
       });
-      setTodos((prevTodos) => {
-        const updatedTodos = [...prevTodos];
-        const index = updatedTodos.findIndex((todo) => todo._id === id);
-        updatedTodos[index] = response.data;
-        return updatedTodos;
-      });
-      setUpdateTitle('');
-      setUpdateDescription('');
-      setSelectedTodo(null); // Reset selected todo
+      const updatedTodos = todos.map(todo =>
+        todo._id === response.data._id ? response.data : todo
+      );
+      setTodos(updatedTodos);
+      setTitle('');
+      setDescription('');
+      setEditingTodo(null);
     } catch (error) {
       console.log('Failed to update todo:', error);
     }
   };
 
-  const deleteTodo = async (id) => {
-    setSelectedTodo(id);
-    setShowDeleteDialog(true);
+  const handleDeleteConfirmation = todo => {
+    setEditingTodo(todo);
+    setShowDeleteConfirmation(true);
   };
 
-  const confirmDelete = async () => {
+  const handleDeleteTodo = async () => {
     try {
-      await Axios.delete(`http://localhost:4000/todos/${selectedTodo}`);
-      setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== selectedTodo));
-      setShowDeleteDialog(false);
+      await axios.delete(`http://localhost:4000/todos/${editingTodo._id}`);
+      const updatedTodos = todos.filter(todo => todo._id !== editingTodo._id);
+      setTodos(updatedTodos);
+      setEditingTodo(null);
+      setShowDeleteConfirmation(false);
     } catch (error) {
       console.log('Failed to delete todo:', error);
     }
   };
 
-  return (
-    <div className="App">
-      <h2>To-Do App</h2>
-      <div className="form-container">
-        {selectedTodo ? (
-          <>
-            <label htmlFor="updateTitle">Task:</label>
-            <input
-              id="updateTitle"
-              type="text"
-              value={updateTitle}
-              onChange={(event) => setUpdateTitle(event.target.value)}
-            />
-            <label htmlFor="updateDescription">Description:</label>
-            <input
-              id="updateDescription"
-              type="text"
-              value={updateDescription}
-              onChange={(event) => setUpdateDescription(event.target.value)}
-            />
-          </>
-        ) : (
-          <>
-            <label htmlFor="title">Task:</label>
-            <input
-              id="title"
-              type="text"
-              value={updateTitle}
-              onChange={(event) => setUpdateTitle(event.target.value)}
-            />
-            <label htmlFor="description">Description:</label>
-            <input
-              id="description"
-              type="text"
-              value={updateDescription}
-              onChange={(event) => setUpdateDescription(event.target.value)}
-            />
-          </>
-        )}
-        <button onClick={selectedTodo ? () => updateTodo(selectedTodo) : addTodo}>
-          {selectedTodo ? 'Update Todo' : 'Add Todo'}
-        </button>
-      </div>
-      <h2>Todo List</h2>
-      {todos.map((todo) => (
-        <div key={todo._id} className="card">
+  const renderTodos = () => {
+    return todos.map(todo => (
+      <div key={todo._id} className="todo-card">
+        <div className="todo-info">
           <h3>{todo.title}</h3>
           <p>{todo.description}</p>
-          <div className="button-container">
-            <button
-              className="update-button"
-              onClick={() => {
-                setSelectedTodo(todo._id);
-                setUpdateTitle(todo.title);
-                setUpdateDescription(todo.description);
-              }}
-            >
-              Update
-            </button>
-            <button className="delete-button" onClick={() => deleteTodo(todo._id)}>
-              Delete
-            </button>
-          </div>
         </div>
-      ))}
+        <div className="todo-actions">
+          <button onClick={() => handleEditTodo(todo)}>Edit</button>
+          <button onClick={() => handleDeleteConfirmation(todo)}>Delete</button>
+        </div>
+      </div>
+    ));
+  };
 
-      {/* Delete Confirmation Dialog */}
-      {showDeleteDialog && (
-        <div className="dialog">
-          <p>Are you sure you want to delete the task?</p>
-          <div className="dialog-buttons">
-            <button onClick={() => setShowDeleteDialog(false)}>Cancel</button>
-            <button onClick={() => confirmDelete()}>Delete</button>
-          </div>
+  const handleEditTodo = todo => {
+    setTitle(todo.title);
+    setDescription(todo.description);
+    setEditingTodo(todo);
+  };
+
+  return (
+    <div>
+      <div className="add-todo">
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
+        {!editingTodo && !showDeleteConfirmation ? (
+          <button onClick={handleCreateTodo}>Add</button>
+        ) : (
+          <>
+            {editingTodo && !showDeleteConfirmation ? (
+              <>
+                <button onClick={handleUpdateTodo}>Update</button>
+                <button onClick={() => setEditingTodo(null)}>Cancel</button>
+              </>
+            ) : null}
+          </>
+        )}
+      </div>
+
+      {renderTodos()}
+
+      {showDeleteConfirmation && (
+        <div className="delete-confirmation">
+          <p>Are you sure you want to delete the record?</p>
+          <button onClick={handleDeleteTodo}>Yes</button>
+          <button onClick={() => setShowDeleteConfirmation(false)}>No</button>
         </div>
       )}
     </div>
   );
-}
+};
+
+export default Page;
